74f844c5f62f91df13b96cbaadd58b97d8dd30ae
Titouan Lacombe--Fabre <titouan.lacombe---fabre@etu.univ-cotedazur.fr>
2026-03-05 11:45:49 +0100

    feat: initialize frontend project with React, Tailwind CSS, and essential configurations

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+    return (
+        <div className="flex min-h-svh w-full flex-col items-center justify-center">
+            <Button variant={'default'}>Click me</Button>
+            <div className={'bg-secondary'}>
+                <h1 className="text-2xl font-bold text-primary">Hello, world!</h1>
+            </div>
+        </div>
+    )
+}
+
+export default App
