8997a90dade6aaa1cebf5b325a3893cdc2780d3c
Tamas Palotas <tamas.palotas@etu.univ-cotedazur.fr>
2026-03-18 13:55:30 +0100

    feat: setup fuzzy search

--- frontend/src/pages/home/useHomePageViewModel.ts
+++ frontend/src/pages/home/useHomePageViewModel.ts
@@ -1,33 +1,45 @@
 import { useAnimeStore, type AnimeItemDTO } from "@/stores/animeStore";
 import { useEffect, useState } from "react";
+import Fuse from "fuse.js";
 
-function filterAnimeList(animeList: AnimeItemDTO[], query: string) {
+function filterAnimeList(fuse: Fuse<AnimeItemDTO>, query: string): AnimeItemDTO[] {
     if (!query) return [];
-    const lowerCaseQuery = query.toLowerCase();
-    return animeList.filter((anime: AnimeItemDTO) => {
-        const titleMatch = anime.title.toLowerCase().includes(lowerCaseQuery);
-        const alternativeTitleMatch = anime.alias.some((altTitle: string) => altTitle.toLowerCase().includes(lowerCaseQuery));
-        return titleMatch || alternativeTitleMatch;
-    });
+    const results = fuse.search(query);
+    return results.map((result: any) => result.item);
+}
+
+function createFuse(animeList: AnimeItemDTO[]): Fuse<AnimeItemDTO> {
+    return new Fuse(
+        animeList,
+        {
+            keys: [
+                { name: "title", weight: 0.7 },
+                { name: "alias", weight: 0.3 },
+            ],
+            threshold: 0.3,
+            ignoreLocation: true,
+        }
+    );
 }
 
 export function useHomePageViewModel() {
     const animeStore = useAnimeStore();
     const [isGuessingStarted, setIsGuessingStarted] = useState(false);
     const [inputValue, setInputValue] = useState("");
+    const [fuse, setFuse] = useState<Fuse<AnimeItemDTO>>(createFuse(animeStore.animeList));
 
     useEffect(() => {
         if(animeStore.animeList.length === 0)
             animeStore.loadAnimeList();
     }, []);
 
-
-    
     useEffect(() => {
-    }, [inputValue]);
+        setFuse(createFuse(animeStore.animeList));
+    }, [animeStore.animeList]);
+ 
 
     return {
-        filtredAnimeList: filterAnimeList(animeStore.animeList, inputValue),
+        filtredAnimeList: filterAnimeList(fuse, inputValue),
         isGuessingStarted,
         setIsGuessingStarted,
         inputValue,
